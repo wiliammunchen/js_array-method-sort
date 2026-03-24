6c61bb02f01c58fcb1267b54107ddd033bbb6905
wiliammunchen <wiliam.munchen@gmail.com>
2026-03-24 01:13:35 -0300

    feat: implement custom sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,36 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const compare =
+      compareFunction ||
+      ((firstElement, secondElement) => {
+        const firstString = String(firstElement);
+        const secondString = String(secondElement);
+
+        if (firstString > secondString) {
+          return 1;
+        }
+
+        if (firstString < secondString) {
+          return -1;
+        }
+
+        return 0;
+      });
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
+          const currentValue = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = currentValue;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
